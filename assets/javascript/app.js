0e8146e9c394ff38114752c5b436cc4d77825939
James <semaj5588@gmail.com>
2016-03-03 18:29:05 -0500

    added homework

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+//Struggling with javascript
+
+
+var correct = 0;
+var incorrect = 0;
+
+
+
+
+var seconds = 30;
+function secondPassed() {
+    var minutes = Math.round((seconds - 30)/60);
+    var remainingSeconds = seconds % 60;
+    if (remainingSeconds < 10) {
+        remainingSeconds = "0" + remainingSeconds;  
+    }
+    document.getElementById('countdown').innerHTML = minutes + ":" + remainingSeconds;
+    if (seconds == 0) {
+        clearInterval(countdownTimer);
+        document.getElementById('countdown').innerHTML = "Buzzer";
+    } else {
+        seconds--;
+    }
+}
+ 
